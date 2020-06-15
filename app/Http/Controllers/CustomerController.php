83af24ee37e7d6f26e902be14a58e9ddc98b4798
Ruslan Agaev <ruslanagaev@list.ru>
2020-06-15 03:38:00 +0300

    CRM system

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function getIndex() {
+        $id = Input::get('id');
+        return Customer::find($id);
+    }
+    public function getAll() {
+        return Customer::all();
+    }
+    public function postIndex()
+    {
+        if (Input::has('first_name', 'last_name', 'email')) {
+            $input = Input::all();
+        }
+        if ($input['first_name'] == '' || $input['last_name'] == '' || $input['email'] == '') {
+            return Response::make('You need to fill all of the input fields', 400);
+            $customer = new Customer;
+            $customer->first_name = $input['first_name'];
+            $customer->last_name = $input['last_name'];
+            $customer->email = $input['email'];
+            $customer->save();
+            return $customer;
+
+        } else {
+            return Response::make('You need to fill all of the input fields', 400);
+        }
+
+    }
+    public function deleteIndex() {
+        $id = Input::get('id');
+        $customer = Customer::find($id);
+        $customer->delete();
+        return $id;
+    }
+}
+
---
+++ resources/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html ng-app="CustomerApp">
+<head>
+    <title>Customer Management</title>
+    <link rel="stylesheet" href="../.././css/style.css">
+    <script src="http://code.angularjs.org/1.7.5/angular.js"></script>
+    <script src="http://code.angularjs.org/1.7.5/angular-route.js"></script>
+    <script src="../.././js/CustomerApp.js"></script>
+</head >
+<ng-view></ng-view>
+</html>
